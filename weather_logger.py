865d5cb29753791cbf7a803ffda5b340e2276d2d
Tomasz Kozlowski <tomasz.kozlowski@mycronic.com>
2021-06-15 11:22:38 +0200

    Update weather_logger.py

--- weather_logger.py
+++ weather_logger.py
@@ -15,121 +15,104 @@ class IFDB(object):
         self.password = password
         self.database = database
 
-    def add_points(self, data):
+    def add_points(self, time_data_list):
         # self._connect()
-        self._format_and_write(data)
+        for element in time_data_list:
+            self._format_and_write(element[0], element[1])
         # self.client.close()
 
     # def _connect(self):
-    #     self.client = InfluxDBClient(host="localhost",
-    #                                  port=8086,
-    #                                  username=self.username,
+    #     self.client = InfluxDBClient(username=self.username,
     #                                  password=self.password,
-    #                                  database=self.database
-    #                                  )
+    #                                  database=self.database)
 
-    def _format_and_write(self, data):
-        """Formatting data for the upload. Uses "data" which is subclass
-        of sensors class defined in main logger script"""
-        body = [
-                    {
-                        "measurement": self.measurement,
-                        "time": data.dt,
-                        "fields": {
-                            "longitude": data.longitude,
-                            "latitude": data.latitude,
-                            "weather_id": data.weather_id,
-                            "weather_category": data.weather_category,
-                            "weather_description": data.weather_description,
-                            "weather_icon": data.weather_icon,
-                            "temperature": data.temperature,
-                            "feels_like": data.feels_like,
-                            "temp_min": data.temp_min,
-                            "temp_max": data.temp_max,
-                            "pressure": data.pressure,
-                            "humidity": data.humidity,
-                            "wind_speed": data.wind_speed,
-                            "wind_angle": data.wind_angle,
-                            "clouds": data.clouds,
-                            "location": data.location,
-                            "uv_index": data.uvi
-                        }
-                    }
-                ]
+    def _format_and_write(self, time, data_dict):
+        """time must be in epoch in ns format, data_dict comtains names
+        of fields and their values"""
+        body = [{"measurement": self.measurement,
+                 "time": time,
+                 "fields": data_dict}]
 
         # self.client.write_points(body)
         return body
 
 
-class container(object):
-    pass
-
-
 class OpenWeather(object):
 
     def __init__(self):
-        self.data = container()
+        self.now = dict()
+        self.forcast = list()
 
     def update(self):
-        r = requests.get('https://api.openweathermap.org/data/2.5/'
-                         'weather?'
-                         '&lat=59.36142886509909'
-                         '&lon=18.008168552706497'
-                         '&units=metric'
-                         '&APPID=33f6aa60676a838926b064aca4cadbdd')
-        if len(r.json()) < 5:
-            raise Exception(f'Error! {r.json()["message"]}')
-        self.pack = r.json()
+        response = requests.get('https://api.openweathermap.org/data/2.5/'
+                                'weather?'
+                                '&lat=59.36142886509909'
+                                '&lon=18.008168552706497'
+                                '&units=metric'
+                                '&APPID=33f6aa60676a838926b064aca4cadbdd')
+        if len(response.json()) < 5:
+            raise Exception(f'Error! {response.json()["message"]}')
+        self.package = response.json()
 
     def pack_data(self):
-        self.data.latitude = float(self.pack['lat'])
-        self.data.longitude = float(self.pack['lon'])
+        p = self.package
 
-        self.data.dt = int(self.pack['current']['dt'] * 1e9)
-        self.data.temperature = float(self.pack['current']['temp'])
-        self.data.feels_like = float(self.pack['current']['feels_like'])
-        self.data.pressure = float(self.pack['current']['pressure'])
-        self.data.humidity = float(self.pack['current']['humidity'])
-        self.data.uvi = float(self.pack['current']['uvi'])
-        self.data.clouds = float(self.pack['current']['clouds'])
-        self.data.wind_speed = float(self.pack['current']['wind_speed'])
-        self.data.wind_angle = float(self.pack['current']['wind_deg'])
+        self.now_dt = int(p['current']['dt'] * 1e9)
 
-        self.data.weather_id = int(self.pack['current']['weather'][0]['id'])
-        self.data.weather_category = self.pack['current']['weather'][0]['main']
-        self.data.weather_description = self.pack['current']['weather'][0]['description']
-        self.data.weather_icon = self.pack['current']['weather'][0]['icon']
+        self.now['latitude'] = float(p['lat'])
+        self.now['longitude'] = float(p['lon'])
 
-        self.data.temp_max = float(self.pack['daily'][0]['temp']['max'])
-        self.data.temp_min = float(self.pack['daily'][0]['temp']['min'])
+        self.now['temperature'] = float(p['current']['temp'])
+        self.now['feels_like'] = float(p['current']['feels_like'])
+        self.now['pressure'] = float(p['current']['pressure'])
+        self.now['humidity'] = float(p['current']['humidity'])
 
-        for i in range(12):
-            globals()['fig%s' % x] = 'values'
+        self.now['uvi'] = float(p['current']['uvi'])
+        self.now['clouds'] = float(p['current']['clouds'])
+        self.now['wind_speed'] = float(p['current']['wind_speed'])
+        self.now['wind_angle'] = float(p['current']['wind_deg'])
 
-        # FIELDS is a dictionary!
+        self.now['id'] = int(p['current']['weather'][0]['id'])
+        self.now['category'] = p['current']['weather'][0]['main']
+        self.now['description'] = p['current']['weather'][0]['description']
+        self.now['icon'] = p['current']['weather'][0]['icon']
 
-        # d = {}
-        # for x in range(1, 10):
-        #     d["string{0}".format(x)] = "Hello"
+        # !!! Verify [0] always gives 'today'
+        self.now['temp_max'] = float(p['daily'][0]['temp']['max'])
+        self.now['temp_min'] = float(p['daily'][0]['temp']['min'])
 
+        # !!!
+        """ Check if the dt has fixed time and only date changes.
+        In such a case every time i check I will update timestamps
+        (database IDs) and only add a new point every hour. This way I can
+        have values linked to time (easy for visualizations) """
+        for h in range(48):
 
-        # Pop (hourly)
+            t = int(p['hourly'][h]['dt'] * 1e9)
+
+            data_dict = {'temperature': float(p['hourly'][h]['temp']),
+                        'pop': float(p['hourly'][h]['pop'])}
+
+            self.forcast.append([t, data_dict])
 
 
 delay = 5
 db = IFDB('weather', 'grafana', 'raspberrypi', 'overseer')
+weather = OpenWeather()
 reference = time.time()
-alldata = list()
+alldata = list()  # TESTS ONLY
+alldata2 = list()  # TESTS ONLY
 
 while True:
 
     if (time.time() - reference) <= delay:
         pass
     else:
-        weather = OpenWeather()
         weather.update()
         # weather.pack_data()
-        # alldata.append(db._format_and_write(weather.data))
-        # db.add_points(weather.data)
+        # alldata.append(db._format_and_write(weather.now_dt, weather.now))  # TESTS ONLY
+        # alldata2.append(db._format_and_write(weather.now_dt, weather.forcast))  # TESTS ONLY
+        # db.add_points([weather.now_dt, weather.now])
+        # db.add_points(weather.forcast)
         reference = time.time()
         print('Point saved')
