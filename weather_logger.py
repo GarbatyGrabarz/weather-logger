a4356220e7c9958f16eed0dfc11ef2ace5a5e36e
Tomasz <garbatygrabarz@gmail.com>
2021-06-16 22:00:08 +0200

    Update weather_logger.py

--- weather_logger.py
+++ weather_logger.py
@@ -1,8 +1,7 @@
 import pytz
 import requests
 import time
-import logging
-# from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient
 
 
 class IFDB(object):
@@ -16,15 +15,16 @@ class IFDB(object):
         self.database = database
 
     def add_points(self, time_data_list):
-        # self._connect()
+        self._connect()
         for element in time_data_list:
+            print(element)
             self._format_and_write(element[0], element[1])
-        # self.client.close()
+        self.client.close()
 
-    # def _connect(self):
-    #     self.client = InfluxDBClient(username=self.username,
-    #                                  password=self.password,
-    #                                  database=self.database)
+    def _connect(self):
+        self.client = InfluxDBClient(username=self.username,
+                                     password=self.password,
+                                     database=self.database)
 
     def _format_and_write(self, time, data_dict):
         """time must be in epoch in ns format, data_dict comtains names
@@ -33,8 +33,7 @@ class IFDB(object):
                  "time": time,
                  "fields": data_dict}]
 
-        # self.client.write_points(body)
-        return body
+        self.client.write_points(body)
 
 
 class OpenWeather(object):
@@ -45,13 +44,17 @@ class OpenWeather(object):
 
     def update(self):
         response = requests.get('https://api.openweathermap.org/data/2.5/'
-                                'weather?'
+                                'onecall?'
                                 '&lat=59.36142886509909'
                                 '&lon=18.008168552706497'
                                 '&units=metric'
                                 '&APPID=33f6aa60676a838926b064aca4cadbdd')
         if len(response.json()) < 5:
-            raise Exception(f'Error! {response.json()["message"]}')
+            try:
+                print(f'Error! {response.json()["message"]}')
+            except KeyError:
+                print('Something went wrong but there is no error for it')
+
         self.package = response.json()
 
     def pack_data(self):
@@ -77,31 +80,23 @@ class OpenWeather(object):
         self.now['description'] = p['current']['weather'][0]['description']
         self.now['icon'] = p['current']['weather'][0]['icon']
 
-        # !!! Verify [0] always gives 'today'
         self.now['temp_max'] = float(p['daily'][0]['temp']['max'])
         self.now['temp_min'] = float(p['daily'][0]['temp']['min'])
 
-        # !!!
-        """ Check if the dt has fixed time and only date changes.
-        In such a case every time i check I will update timestamps
-        (database IDs) and only add a new point every hour. This way I can
-        have values linked to time (easy for visualizations) """
         for h in range(48):
-
             t = int(p['hourly'][h]['dt'] * 1e9)
 
             data_dict = {'temperature': float(p['hourly'][h]['temp']),
-                        'pop': float(p['hourly'][h]['pop'])}
+                         'pop': float(p['hourly'][h]['pop'])}
 
             self.forcast.append([t, data_dict])
 
 
 delay = 5
-db = IFDB('weather', 'grafana', 'raspberrypi', 'overseer')
+db_weather = IFDB('weather', 'grafana', 'raspberrypi', 'overseer')
+db_forcast = IFDB('forcast', 'grafana', 'raspberrypi', 'overseer')
 weather = OpenWeather()
 reference = time.time()
-alldata = list()  # TESTS ONLY
-alldata2 = list()  # TESTS ONLY
 
 while True:
 
@@ -109,10 +104,8 @@ while True:
         pass
     else:
         weather.update()
-        # weather.pack_data()
-        # alldata.append(db._format_and_write(weather.now_dt, weather.now))  # TESTS ONLY
-        # alldata2.append(db._format_and_write(weather.now_dt, weather.forcast))  # TESTS ONLY
-        # db.add_points([weather.now_dt, weather.now])
-        # db.add_points(weather.forcast)
+        weather.pack_data()
+        db_weather.add_points([[weather.now_dt, weather.now]])
+        db_forcast.add_points(weather.forcast)
         reference = time.time()
         print('Point saved')
